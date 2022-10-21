3961f87f9e3e067823291a2895590d2655d673f9
jordangm94 <jordan.guerrero.martinez@gmail.com>
2022-10-21 00:06:48 +0000

    Added  Get Reservation function and appropraite query within function to database.js

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -86,17 +86,6 @@ exports.getUserWithId = getUserWithId;
     });
 };
 
-
-
-
-
-
-// const addUser =  function(user) {
-//   const userId = Object.keys(users).length + 1;
-//   user.id = userId;
-//   users[userId] = user;
-//   return Promise.resolve(user);
-// }
 exports.addUser = addUser;
 
 /// Reservations
@@ -106,9 +95,28 @@ exports.addUser = addUser;
  * @param {string} guest_id The id of the user.
  * @return {Promise<[{}]>} A promise to the reservations.
  */
-const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+ const getAllReservations = function(guest_id, limit = 10) {
+  return pool.query(
+    `SELECT properties.*, reservations.*, avg(rating) as average_rating
+    FROM reservations
+    JOIN properties ON reservations.property_id = properties.id
+    JOIN property_reviews ON properties.id = property_reviews.property_id
+    WHERE reservations.guest_id = $1
+    GROUP BY properties.id, reservations.id
+    ORDER BY reservations.start_date
+    LIMIT $2;`, [guest_id, limit])
+    .then(result => result.rows) //=> {
+    //   console.log(result);
+    //   return result.rows;
+    // })
+    // .catch((err) => {
+    //   console.log(err.message);
+    // });
 }
+
+// const getAllReservations = function(guest_id, limit = 10) {
+//   return getAllProperties(null, 2);
+// }
 exports.getAllReservations = getAllReservations;
 
 /// Properties
